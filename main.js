839085d2f9b62ad4d4bd1ebd8529d78f837d5e73
Xhiomi <xhiomi@gmail.com>
2019-02-11 20:26:14 -0600

    Se agregó comentario

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 var cowsay = require('cowsay');
 
+let historia = 'Hola, mi nombre es ' + nombre +
+'. Y hoy practicaremos el uso de las: ' + temaVistoHoy + '. Vimos else {
+  ejemplo de una vaquita que decía ' fraseDeVaquita + '.';
+let nombre = 'Fanny';
+let temaVistoHoy = 'variables'
+let fraseDeVaquita = '¡Hola mundo!'
+
+console.log(historia)
+
 console.log(cowsay.say({
-	text : "Hola",
+	text : fraseDeVaquita,
 	e : "uu",
 	T : "w"
 }));
+
+cowsay.think()
 
 // console.log('hola ' + (8+9))
